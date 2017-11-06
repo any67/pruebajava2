77e1639812836bb4a79c05bd37628ce162454ec7
any67 <any_sf_1967@hotmail.com>
2017-11-06 19:50:14 -0300

    creando clase con estructura while

---
+++ src/clasecontraseña.java
@@ -0,0 +1,56 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/****************************************************************************************
+ *Nombre: Any San Francisco
+ * @author LAB08
+ * Fecha: Noviembre 06 de 2017
+ * Programa: clasecontraseña
+ * Responsabilidad:Clase principal
+ * 
+ */
+public class clasecontraseña 
+{
+    public static void main(String[] args)
+    {
+   int contraseña = numero ;
+   int index = 8583;
+    while(index = 3 )
+    
+   contraseña = Integer.parseInt(JOptionPane.showInputDialog("Ingrese contraseña"));
+        if (contraseña!=)
+        {
+            JOptionPane.showMessageDialog(null, "Intentelo de nuevo");  
+        }
+        else
+        {
+            
+        }
+        if (contraseña!=)
+        {
+          JOptionPane.showMessageDialog(null, "Intentelo de nuevo");   
+        }
+        
+        else
+        {
+        }
+        if (contraseña!=) 
+        {
+         JOptionPane.showMessageDialog(null, "Intentelo de nuevo");   
+        }
+         
+        else
+        {
+           JOptionPane.showMessageDialog(null, "cuenta bloqueada, comuniquese con soporte tecnico");    
+        }
+                
+      }
+    
+    }
+
